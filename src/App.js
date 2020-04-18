df7617d6c16b80e31681aa3071fade864cb11271
bumsu <bumsu0211@gmail.com>
2020-04-19 00:29:50 +0900

    Improvement: Install PropTypes and check props validations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Movie({ title, year }) {
+function Movie({ title, year, rating }) {
   console.log(title);
   console.log(year);
   return (
-    <h3>
-      {title} : {year}
-    </h3>
+    <div>
+      <h3>
+        {title} : {year}
+      </h3>
+      <p>{rating}</p>
+    </div>
   );
 }
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const movies = [
   {
     id: 1,
     title: "Life of Pi",
     year: 2012,
+    rating: 5,
   },
   {
     id: 2,
     title: "Avengers(End Game)",
     year: 2019,
+    rating: 4.5,
   },
 ];
 
@@ -28,7 +40,14 @@ function App() {
     <div className="App">
       <h1>App</h1>
       {movies.map((movie) => {
-        return <Movie title={movie.title} year={movie.year} key={movie.id} />;
+        return (
+          <Movie
+            title={movie.title}
+            year={movie.year}
+            rating={movie.rating}
+            key={movie.id}
+          />
+        );
       })}
     </div>
   );
