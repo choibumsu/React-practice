f9392429f3b568d12d5b1de47163c0ac0e3ebd30
bumsu <bumsu0211@gmail.com>
2020-04-18 17:43:01 +0900

    Feature: Use Props to child component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Movie({ title, year }) {
+  console.log(title);
+  console.log(year);
+  return (
+    <h3>
+      {title} : {year}
+    </h3>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <Potato />
+      <Movie title="Life of Pi" year="2012" />
+      <Movie title="Avengers(End Game)" year="2019" />
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Potato() {
-  return <h3>Potato</h3>;
-}
-
-export default Potato;
