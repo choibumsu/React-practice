b3d256faf8c5903e44ac11a5264b1918c27f53de
bumsu <bumsu0211@gmail.com>
2020-04-19 16:20:10 +0900

    Imporvement: Styling

--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} />
       <div className="movie-data">
         <h3 className="movie-title">{title}</h3>
         <h5 className="movie-year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li className="genre" key={index}>
+              {genre}
+            </li>
+          ))}
+        </ul>
         <p className="movie-summary">{summary}</p>
       </div>
     </div>
@@ -21,6 +28,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
