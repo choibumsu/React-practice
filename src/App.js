e1800d09b073b81cd2352cb7283a5024c3384065
bumsu <bumsu0211@gmail.com>
2020-04-19 17:17:16 +0900

    Feature: Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" exact={true} component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -52,4 +52,4 @@ class Home extends React.Component {
   }
 }
 
-export default App;
+export default Home;
