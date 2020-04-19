a975af8d28feb0717cef23dfa6cf3462363ad334
bumsu <bumsu0211@gmail.com>
2020-04-19 17:38:53 +0900

    Feature: Add Detail page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} />
-      <div className="movie-data">
-        <h3 className="movie-title">{title}</h3>
-        <h5 className="movie-year">{year}</h5>
-        <ul className="genres">
-          {genres.map((genre, index) => (
-            <li className="genre" key={index}>
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie-summary">{summary.slice(0, 180)}</p>
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} />
+        <div className="movie-data">
+          <h3 className="movie-title">{title}</h3>
+          <h5 className="movie-year">{year}</h5>
+          <ul className="genres">
+            {genres.map((genre, index) => (
+              <li className="genre" key={index}>
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie-summary">{summary.slice(0, 180)}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
---
+++ src/routes/Detail.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function Detail() {}
+
+export default Detail;
