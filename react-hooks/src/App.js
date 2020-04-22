277e30dbce70a25ea43b42d2e03b82bc2392da55
bumsu <bumsu0211@gmail.com>
2020-04-22 15:47:17 +0900

    Improvement: Add validator to useInput and divide to file

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,20 +1,10 @@
-import React, { useState } from "react";
-
+import React from "react";
+import { useInput } from "./hooks/useInput";
 import "./App.css";
 
-const useInput = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setValue(value);
-  };
-  return { value, onChange };
-};
-
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
 
   return (
     <div className="App">
