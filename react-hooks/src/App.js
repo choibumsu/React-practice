86eb279b4f0b22d0321b760005f485baa273e744
bumsu <bumsu0211@gmail.com>
2020-04-22 16:04:33 +0900

    Feature: Create useTabs

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { useInput } from "./hooks/useInput";
+import { useTabs } from "./hooks/useTabs";
 import "./App.css";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
 
 const App = () => {
   const maxLen = (value) => value.length <= 10;
   const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
 
   return (
     <div className="App">
       <h1>Hello</h1>
       <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)} key={index}>
+          {section.tab}
+        </button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
