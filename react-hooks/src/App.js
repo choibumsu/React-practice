10ff28af7b474a3fefc92b5026e0864ca4346e95
bumsu <bumsu0211@gmail.com>
2020-04-22 15:39:29 +0900

    Feature: Create useInput hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 
 import "./App.css";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setValue(value);
+  };
+  return { value, onChange };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-  const increaseItem = () => setItem(item + 1);
-  const decreaseItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
 
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <button onClick={increaseItem}>Increment</button>
-      <button onClick={decreaseItem}>Decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
