536b70cba4a501464172e87f7894e7e8776dc90d
bumsu <bumsu0211@gmail.com>
2020-04-23 15:44:17 +0900

    [Context-api] Use Store

--- context-api/src/Components/App/AppContainer.js
+++ context-api/src/Components/App/AppContainer.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import AppPresenter from "./AppPresenter";
+import Store from "../../store";
 
 class AppContainer extends Component {
+  state = {
+    message: "Hello",
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        message: "Bye",
+      });
+    }, 2000);
+  };
   render() {
-    return <AppPresenter />;
+    return (
+      <Store.Provider value={this.state}>
+        <AppPresenter />
+      </Store.Provider>
+    );
   }
 }
 
