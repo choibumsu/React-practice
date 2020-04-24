8920cc497e6c74fb62c31baf2424d093843eb907
bumsu <bumsu0211@gmail.com>
2020-04-25 01:29:06 +0900

    [Next-js] Fetching Data on Server Side and Client Side

--- next-js/pages/post.js
+++ next-js/pages/post.js
@@ -1,14 +1,22 @@
 import Layout from "../components/MyLayout.js";
+import fetch from "isomorphic-unfetch";
 
-const Content = (props) => (
-  <div>
-    <h1>{props.url.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-);
-
-export default (props) => (
+const Post = (props) => (
   <Layout>
-    <Content url={props.url} />
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p>
+    <img src={props.show.image.medium} />
   </Layout>
 );
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
